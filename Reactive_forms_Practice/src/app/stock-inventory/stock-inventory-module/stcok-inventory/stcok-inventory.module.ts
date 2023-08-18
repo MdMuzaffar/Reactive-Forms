21bb98bbb856d692c1748ece78db936a94ef01de
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2023-08-18 23:14:20 +0530

    Reactive_forms
    Setup to practice reactive forms

---
+++ Reactive_forms_Practice/src/app/stock-inventory/stock-inventory-module/stcok-inventory/stcok-inventory.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {StockInventoryComponent} from './../../container/stock-inventory/stock-inventory.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [StockInventoryComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  exports:[
+    StockInventoryComponent
+  ]
+})
+export class StockInventoryModule { }
